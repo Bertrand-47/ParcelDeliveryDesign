d7fe337b2bfcdc163e9c7542e0c4fb6842966808
sibomana47 <46084008+sibomana47@users.noreply.github.com>
2018-12-23 13:22:46 +0200

    Add files via upload

---
+++ js/Hider.js
@@ -0,0 +1,14 @@
+function Registration()
+{
+		const element = document.querySelector(".Registration-form"); // to give a class to the button
+		const element2=document.querySelector(".login-form");
+		var unhide=element.style.display="block";
+		var hider=element2.style.display="none";
+}
+function login()
+{
+		const element = document.querySelector(".login-form"); // to give a class to the button
+		const element2 = document.querySelector(".Registration-form"); // to give a class to the button
+		var unhide=element.style.display="block";
+		var hider=element2.style.display="none";
+}
